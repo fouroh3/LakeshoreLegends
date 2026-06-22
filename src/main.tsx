9cb2849c30a75b991f36037d66c0a7c8485ed577
fouroh3 <danielpolsky@me.com>
2026-06-22 14:22:10 -0600

    Load Final Examiner live ticker

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import "./finalExaminerCritical.css";
 import "./finalExaminerBossCards.css";
 import "./finalExaminerMinionEffects.css";
 import "./finalExaminerClassDanger.css";
+import "./finalExaminerLiveTicker";
 
 const isFinalExaminerRoute =
   window.location.pathname.replace(/^\/+|\/+$/g, "").toLowerCase() ===
