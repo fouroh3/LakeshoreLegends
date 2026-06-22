3108bba1b66a230eed0f0b66c59d42d42d621bd3
fouroh3 <danielpolsky@me.com>
2026-06-22 10:34:52 -0600

    Route Final Examiner raid page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import FinalExaminerRaid from "./pages/battle/FinalExaminerRaid";
 import "./index.css";
+
+const isFinalExaminerRoute =
+  window.location.pathname.replace(/^\/+|\/+$/g, "").toLowerCase() ===
+  "finalexaminer";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {isFinalExaminerRoute ? <FinalExaminerRaid /> : <App />}
   </React.StrictMode>
 );
