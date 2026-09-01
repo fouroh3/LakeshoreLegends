828e894288f264149e8c7819aa01bb0becb779d4
fouroh3 <danielpolsky@me.com>
2026-09-01 14:34:49 -0600

    Proxy Apps Script in local development

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const APPS_SCRIPT_PATH =
+  "/macros/s/AKfycbw6gMIFYPvaljF3Ls-waojzprU6bygZZonOIJeKLopN2NSKgkDT-EsRKznxQiGpth_6/exec";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/ll-api": {
+        target: "https://script.google.com",
+        changeOrigin: true,
+        followRedirects: true,
+        rewrite: (path) => path.replace(/^\/ll-api/, APPS_SCRIPT_PATH),
+      },
+    },
+  },
 });
